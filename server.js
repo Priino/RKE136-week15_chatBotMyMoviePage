154b5152f951ea8522e78467a15b46176f2c0119
Priit P. <priit.printsmann@tktk.ee>
2024-04-13 20:49:27 +0300

    ,,,

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/getmovie', (req, res) => {
 			: '';
 
 	const reqUrl = encodeURI(
-		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9dib`
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=ccf9dib`
 	);
 	http.get(
 		reqUrl,
